414c7d7f77a1940568177bcf9f8a0440f3d0b1fa
Felipe de Moraes <felipe.moraes@playkids.com>
2020-01-07 09:12:07 -0300

    First commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,17 @@
+$(function() {
+    $('#lightSlider').lightSlider({
+        gallery: true,
+        item: 1,
+        slideMargin: 0,
+        thumbItem: 4,
+        autoWidth: false,
+
+        galleryMargin: 20,
+        thumbMargin: 15,
+        currentPagerPosition: 'middle',
+        speed: 1000,
+        auto: true,
+        loop: true,
+        pause: 3000,
+    });
+});
