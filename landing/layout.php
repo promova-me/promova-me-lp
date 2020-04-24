771e07d27726946b1ed9e78a2c9e5890b5c66bae
Felipe de Moraes <felipe.moraes@playkids.com>
2020-04-24 12:23:57 -0300

    add meta tags and og tags

--- landing/layout.php
+++ landing/layout.php
@@ -6,6 +6,22 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
     <title>Promova.me | Crie grátis uma imagem promocional para seu negócio</title>
 
+    <!--  Meta Tags  -->
+    <meta name="description" content="Crie gratuitamente, online e com poucos cliques uma imagem personalizada para divulgação do seu negócio.">
+    <meta name="keywords" content="divulgar, negócio, pequeno negócio, pequeno empreendedor, empreendedor, negócio, cartão promocional, empreender, propaganda, card promocional, imagem personalizada">
+    <meta name="language" content="Portuguese">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+    <!--  OpenGraph Tags  -->
+    <meta property="og:locale" content="pt_BR">
+    <meta property="og:url" content="https://www.promova.me">
+    <meta property="og:title" content="Promova.me | Crie grátis uma imagem promocional para seu negócio">
+    <meta property="og:site_name" content="Promova.me">
+    <meta property="og:description" content="Crie gratuitamente, online e com poucos cliques uma imagem personalizada para divulgação do seu negócio.">
+    <meta property="og:image" content="www.promova.me/assets/images/logo-promova-me.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:type" content="website">
+
     <!--  Bootstrap & JQuery  -->
     <script src="/assets/js/jquery.min.js"></script>
     <script src="/assets/js/jquery.mask.min.js"></script>
