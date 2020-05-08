f685bf8755e0f5d08bfeabaa6279c6fd9b0f7546
Nykolas Fornaziero <nyfornaziero@gmail.com>
2020-05-07 21:45:16 -0300

    update source and phone mask

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
-
+$cookie_name = "src";
+$cookie_value = $_GET['src'];
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
 require('landing/layout.php');
--- landing/layout.php
+++ landing/layout.php
@@ -1,11 +1,4 @@
 <!DOCTYPE html>
-<?php
-
-$cookie_name = "src";
-$cookie_value = $_GET['src'];
-session_start();
-setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
-?>
 <html lang="pt-br">
 <head>
     <!-- Google Tag Manager -->
