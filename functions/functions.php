e6aab1c349517de7c9c6b32bd9fbc70196ad6f96
Nykolas Fornaziero <nyfornaziero@gmail.com>
2020-05-07 21:41:59 -0300

    update source and phone mask

--- functions/functions.php
+++ functions/functions.php
@@ -1,8 +1,8 @@
 <?php
 //var_dump($_POST);
 try {
-//    $pdo = new PDO('mysql:host=localhost;dbname=promovam_db', 'promovam_admin', '@pR0mOv4M3#');
-    $pdo = new PDO('mysql:host=localhost;dbname=promovam_db', 'root', '');
+    $pdo = new PDO('mysql:host=localhost;dbname=promovam_db', 'promovam_admin', '@pR0mOv4M3#');
+//    $pdo = new PDO('mysql:host=localhost;dbname=promovam_db', 'root', '');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $stmt = $pdo->prepare('INSERT INTO company (name,phone,email,where_deliver,services,newsletter,src)
